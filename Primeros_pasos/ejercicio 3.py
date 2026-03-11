200facee0e22b3f570d69faf845563fd312df396
IgnacioP <ignacioperezfernandez7@gmail.com>
2026-03-11 11:20:04 +0100

    Create ejercicio 3.py

---
+++ Primeros_pasos/ejercicio 3.py
@@ -0,0 +1,12 @@
+
+numero_complejo=complex(float(input("Introduce parte real:")), float(input("Introduce parte imaginaria")))
+print("el numero complejo es:", numero_complejo)
+
+numero_complejo2=complex(float(input("introduce parte real")),float(input("Introduce parte imaginaria")))
+print("el segundo numero complejo es:", numero_complejo2)
+
+
+print("La suma de los numeros complejos es:", numero_complejo+numero_complejo2)
+print("La resta de los numeros complejos es:", numero_complejo-numero_complejo2)
+print("La multiplicacion de los numeros complejos es:", numero_complejo*numero_complejo2)
+print("La división de los numeros complejos es:", numero_complejo/numero_complejo2)
