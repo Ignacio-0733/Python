01bd70d81110ccfc6918b8e3e26a41acda69b498
IgnacioP <ignacioperezfernandez7@gmail.com>
2026-03-11 11:18:44 +0100

    Create ejercicio 1.py

---
+++ Primeros_pasos/ejercicio 1.py
@@ -0,0 +1,8 @@
+#Pedir valores
+dato1=int(input("Teclea el primer valor"))
+dato2=int(input("Teclea el segundo valor valor"))
+div=dato1/dato2
+#Mostramos por pantalla
+print("El primer valor es",dato1)
+print("El segundo valor es",dato2)
+print("el resultado de la division es",dato1 / dato2)
