39b07766906df66b8a482baaa14b5afc20c65cec
IgnacioP <ignacioperezfernandez7@gmail.com>
2026-03-11 11:21:18 +0100

    Create ejercicio if simple.py

---
+++ Condicionales/ejercicio if simple.py
@@ -0,0 +1,20 @@
+num=int(input("Introduce un valor para comprobar si es par o impar"))
+if num%2==0:
+    print("El numero es par")
+else:
+    print("El numero es impar")
+    
+
+valor=input("Introduce el primer valor de tipo texto")
+valor1=input("Introduce el segundo valor de tipo texto")
+valor3= valor+valor1
+print("\"",valor,"\"")
+print("\"",valor1,"\"")
+print("\"",valor3,"\"")
+nota=int(input("Introduce la nota sacada en programación"))
+if nota>=5:
+    print("Has aprobado")
+else:
+    print("has suspendido")
+
+        
