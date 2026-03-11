3e134c2591ef46fab9c3dfc41f6c2a501f4afc2c
IgnacioP <ignacioperezfernandez7@gmail.com>
2026-03-11 11:19:35 +0100

    Create ejercicio 2.py

---
+++ Primeros_pasos/ejercicio 2.py
@@ -0,0 +1,3 @@
+print("Menú:\n\t-Opción 1:Café\n\t-Opción 2:Té\nRuta del archivo:\n\tC:\\Documentos\\Proyectos\\mi_app.py")
+print("Simbolos: \u00A9 \u262f")
+print("Línea 1\n\t\tLínea con doble tab\nEsta es la tercera línea con \'una comilla\' y termina con una comilla doble\"")
