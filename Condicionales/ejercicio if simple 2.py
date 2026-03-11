9d0a8ce36eb5bbe297dcf5db97fd90dadcee8d2d
IgnacioP <ignacioperezfernandez7@gmail.com>
2026-03-11 11:21:52 +0100

    Create ejercicio if simple 2.py

---
+++ Condicionales/ejercicio if simple 2.py
@@ -0,0 +1,32 @@
+num=int(input("Introduce un numero"))
+num1=int(input("Introduce un numero"))
+if num<num1:
+    print("El segundo numero introducido es mayor")
+else:
+     print("El primer numero introducido es mayor")
+
+
+valor=int(input("Introduce un numero"))
+valor1=int(input("Introduce un numero"))
+suma=int(input("Introduce el valor que le quieras sumar al mayor"))
+multiplicar=int(input("Y ahora para la multiplicación"))
+if valor<valor1:
+    print("El segundo numero introducido es mayor:",valor1)
+    valor3=suma+valor1
+    print("La suma da:",valor3)
+    valor4=multiplicar*valor
+    print("La multiplicacion da:",valor4)
+
+if valor1<valor:
+    print("El segundo numero introducido es mayor:",valor1)
+    valor3=suma+valor
+    print("La suma da:",valor3)
+    valor4=multiplicar*valor1
+    print("La multiplicacion da:",valor4)
+    
+    
+         
+
+     
+    
+
